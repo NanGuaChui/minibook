3633714e2c11735a5dc4fcebd00383faa733238d
sgao <sgao@deloitte.com.cn>
2024-12-02 17:34:13 +0800

    增加插件代码

--- src/bookUtil.ts
+++ src/bookUtil.ts
@@ -1,64 +1,53 @@
-import { ExtensionContext, workspace, window } from "vscode";
-import * as fs from "fs";
+import { ExtensionContext, workspace, window } from 'vscode';
+import * as fs from 'fs';
 
 interface BookContent {
   filePath: string;
   chapterList: any[];
 }
 
-export class Book {
+export default class Book {
   currChapterNumber: number = 1;
   currPageNumber: number = 1;
   pageSize: number | undefined = 50;
-  chatper: string = "";
+  chatper: string = '';
   page = 0;
   start = 0;
   end = this.pageSize;
-  filePath: string | undefined = "";
-  bookContent: BookContent = { filePath: "", chapterList: [] };
+  filePath: string | undefined = '';
+  bookContent: BookContent = { filePath: '', chapterList: [] };
 
   constructor() {
-    this.currPageNumber = <number>(
-      workspace.getConfiguration().get("minibook.currPageNumber")
-    );
-    this.currChapterNumber = <number>(
-      workspace.getConfiguration().get("minibook.currChapterNumber")
-    );
-    this.filePath = workspace.getConfiguration().get("minibook.filePath");
-    this.pageSize = workspace.getConfiguration().get("minibook.pageSize");
+    this.currPageNumber = <number>workspace.getConfiguration().get('minibook.currPageNumber');
+    this.currChapterNumber = <number>workspace.getConfiguration().get('minibook.currChapterNumber');
+    this.filePath = workspace.getConfiguration().get('minibook.filePath');
+    this.pageSize = workspace.getConfiguration().get('minibook.pageSize');
 
     this.initBookContent();
   }
 
   loadBook() {
-    this.currPageNumber = <number>(
-      workspace.getConfiguration().get("minibook.currPageNumber")
-    );
-    this.currChapterNumber = <number>(
-      workspace.getConfiguration().get("minibook.currChapterNumber")
-    );
-    this.filePath = workspace.getConfiguration().get("minibook.filePath");
-    this.pageSize = workspace.getConfiguration().get("minibook.pageSize");
+    this.currPageNumber = <number>workspace.getConfiguration().get('minibook.currPageNumber');
+    this.currChapterNumber = <number>workspace.getConfiguration().get('minibook.currChapterNumber');
+    this.filePath = workspace.getConfiguration().get('minibook.filePath');
+    this.pageSize = workspace.getConfiguration().get('minibook.pageSize');
 
     this.initBookContent();
   }
 
   initBookContent() {
-    if (this.filePath === "" || typeof this.filePath === "undefined") {
-      window.showWarningMessage(
-        "请填写TXT格式的小说文件路径 & Please fill in the path of the TXT format novel file"
-      );
+    if (this.filePath === '' || typeof this.filePath === 'undefined') {
+      window.showWarningMessage('请填写TXT格式的小说文件路径 & Please fill in the path of the TXT format novel file');
     }
 
     if (this.filePath === this.bookContent.filePath) {
       return;
     }
 
-    const text: string = fs.readFileSync(this.filePath!, "utf-8");
+    const text: string = fs.readFileSync(this.filePath!, 'utf-8');
 
     // 使用正则表达式匹配章节内容
-    const contentRegex =
-      /(?:.*\s)?第.+章\s*(\S+)([\s\S]*?)(?=(?:.*\s)?第.+章\s*\S+|$)/g;
+    const contentRegex = /(?:.*\s)?第.+章\s*(\S+)([\s\S]*?)(?=(?:.*\s)?第.+章\s*\S+|$)/g;
     let match;
     const chapterList = [];
 
@@ -80,26 +69,22 @@ export class Book {
 
     this.chatper = chapter.content
       .toString()
-      .replace(/\n/g, " ")
-      .replace(/\r/g, " ")
-      .replace(/\t/g, " ")
-      .replace(/　　/g, " ")
-      .replace(/ /g, " ")
+      .replace(/\n/g, ' ')
+      .replace(/\r/g, ' ')
+      .replace(/\t/g, ' ')
+      .replace(/　　/g, ' ')
+      .replace(/ /g, ' ')
       .trim();
   }
 
   getFileName() {
-    const file_name: string | undefined = this.filePath!.split("/").pop();
+    const file_name: string | undefined = this.filePath!.split('/').pop();
     console.log(file_name);
   }
 
   updatePage() {
-    workspace
-      .getConfiguration()
-      .update("minibook.currChapterNumber", this.currChapterNumber, true);
-    workspace
-      .getConfiguration()
-      .update("minibook.currPageNumber", this.currPageNumber, true);
+    workspace.getConfiguration().update('minibook.currChapterNumber', this.currChapterNumber, true);
+    workspace.getConfiguration().update('minibook.currPageNumber', this.currPageNumber, true);
   }
 
   getStartEnd() {
@@ -110,19 +95,18 @@ export class Book {
   }
 
   getTextByPage(): string {
-    if (this.chatper === "") {
+    if (this.chatper === '') {
       this.initChatper();
     }
 
-    this.page =
-      this.bookContent.chapterList[this.currChapterNumber - 1].pageSize;
+    this.page = this.bookContent.chapterList[this.currChapterNumber - 1].pageSize;
 
     const page_info = `${this.currPageNumber}/${this.page}`;
     this.updatePage();
     const { start, end } = this.getStartEnd();
     const currentString = this.chatper.substring(start, end);
 
-    return `(${this.currChapterNumber})${currentString} ${page_info}`;
+    return `(${this.currChapterNumber + 1})${currentString} ${page_info}`;
   }
 
   getPreviousPage(): string {
@@ -132,8 +116,7 @@ export class Book {
 
     if (this.currPageNumber <= 1) {
       this.currChapterNumber -= 1;
-      this.currPageNumber =
-        this.bookContent.chapterList[this.currChapterNumber - 1].pageSize;
+      this.currPageNumber = this.bookContent.chapterList[this.currChapterNumber - 1].pageSize;
       this.initChatper();
     } else {
       this.currPageNumber -= 1;
--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, ExtensionContext, window } from "vscode";
-import * as book from "./bookUtil";
+import * as vscode from 'vscode';
+import Book from './bookUtil';
+import MiniBookViewProvider from './MiniBookView';
 
-let bookInstance: book.Book;
+let bookInstance: Book;
 const getbookInstance = () => {
   if (!bookInstance) {
-    bookInstance = new book.Book();
+    bookInstance = new Book();
   }
   return bookInstance;
 };
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "minibook" is now active!');
@@ -22,38 +23,45 @@ export function activate(context: ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
 
-  // 老板键
-  let displayCode = commands.registerCommand("minibook.displayCode", () => {
-    let lauage_arr_list = ["Mini Book"];
+  // 创建状态栏项
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+  statusBarItem.text = 'Mini Book';
+  statusBarItem.tooltip = 'Mini Book';
+  statusBarItem.command = 'minibook.displayCode';
+  statusBarItem.show();
 
+  // 老板键
+  let displayCode = vscode.commands.registerCommand('minibook.displayCode', () => {
+    let lauage_arr_list = ['Mini Book'];
     var index = Math.floor(Math.random() * lauage_arr_list.length);
-    window.setStatusBarMessage(lauage_arr_list[index]);
+    statusBarItem.text = lauage_arr_list[index];
   });
 
   // 下一页
-  let getNextPage = commands.registerCommand("minibook.getNextPage", () => {
-    window.setStatusBarMessage(getbookInstance().getNextPage());
+  let getNextPage = vscode.commands.registerCommand('minibook.getNextPage', () => {
+    statusBarItem.text = getbookInstance().getNextPage();
   });
 
   // 上一页
-  let getPreviousPage = commands.registerCommand(
-    "minibook.getPreviousPage",
-    () => {
-      window.setStatusBarMessage(getbookInstance().getPreviousPage());
-    }
-  );
+  let getPreviousPage = vscode.commands.registerCommand('minibook.getPreviousPage', () => {
+    statusBarItem.text = getbookInstance().getPreviousPage();
+  });
 
   // 跳转某个页面
-  let getJumpingPage = commands.registerCommand("minibook.reload", () => {
+  let reload = vscode.commands.registerCommand('minibook.reload', () => {
     const book = getbookInstance();
     book.loadBook();
-    window.setStatusBarMessage(book.getTextByPage());
+    statusBarItem.text = book.getTextByPage();
   });
 
   context.subscriptions.push(displayCode);
   context.subscriptions.push(getNextPage);
   context.subscriptions.push(getPreviousPage);
-  context.subscriptions.push(getJumpingPage);
+  context.subscriptions.push(reload);
+
+  // 注册活动栏视图
+  const provider = new MiniBookViewProvider(context.extensionUri);
+  context.subscriptions.push(vscode.window.registerWebviewViewProvider(MiniBookViewProvider.viewType, provider));
 }
 
 // This method is called when your extension is deactivated
